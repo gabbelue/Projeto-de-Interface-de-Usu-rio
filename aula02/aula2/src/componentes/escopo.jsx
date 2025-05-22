32ec57efa4809e7769b215cf4dedae3c2b50100b
gabbelue <m.gabriely@escolar.ifrn.edu.br>
2025-05-22 17:51:58 -0300

    Tarefa de Galeria

---
+++ aula02/aula2/src/componentes/escopo.jsx
@@ -0,0 +1,16 @@
+function Escopo (){
+    const label="Clique aqui"
+    return(
+        <>
+        <div>
+            <h2>Texto de componente</h2>
+        </div>
+        <div>
+            <button>{label}</button>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In commodi, iusto dignissimos quidem nostrum soluta.</p>
+        </div>
+        </>
+    )
+}
+
+export default Escopo 
