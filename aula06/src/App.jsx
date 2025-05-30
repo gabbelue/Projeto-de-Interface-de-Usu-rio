121e1b86b482fbba8cc7b6abb9cf65ca12a2009b
Gabriely <m.gabriely@escolar.ifrn.edu.br>
2025-05-29 21:07:04 -0300

    atividade 06

---
+++ aula06/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MenuDropdown from './componentes/MenuDropdown';
+
+function App(){
+  return(
+    <div>
+      <h1> Meu App React</h1>
+      <MenuDropdown /> {}
+    </div>
+  );
+}
+
+export default App;
